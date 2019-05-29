e03d11598d5cd4b82546588d8e1dfe2f7d02d634
fhoq <iuhiu>
2019-05-29 14:43:01 -0700

    Signed-off-by: fhoq <iuhiu>

---
+++ Dumps/survey_data_entity_questions.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.16  Distrib 10.1.34-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: survey_data
+-- ------------------------------------------------------
+-- Server version	10.1.34-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entity_questions`
+--
+
+DROP TABLE IF EXISTS `entity_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entity_questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(255) DEFAULT NULL,
+  `answer_1` varchar(255) DEFAULT NULL,
+  `answer_2` varchar(255) DEFAULT NULL,
+  `answer_3` varchar(255) DEFAULT NULL,
+  `answer_4` varchar(255) DEFAULT NULL,
+  `answer_5` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entity_questions`
+--
+
+LOCK TABLES `entity_questions` WRITE;
+/*!40000 ALTER TABLE `entity_questions` DISABLE KEYS */;
+INSERT INTO `entity_questions` VALUES (1,'How tall are you?','>6','>5','>4','>3','>2');
+/*!40000 ALTER TABLE `entity_questions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-29 14:42:42
